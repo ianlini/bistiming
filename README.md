e18e816f33a94cf2c422a3a19f6d2c20319ebef5
Ian Lin <andylin514@gmail.com>
2016-10-17 22:53:52 +0800

    finish SimpleTimer examples

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # bistiming
 A timing and progress bar module for Python.
+
+## Examples
+- see [examples/simple_timing_example.py](examples/simple_timing_example.py) for the examples for `SimpleTimer`
+- output of executing `python examples/simple_timing_example.py`:
+  ```
+  [Example 1]
+  ...
+  ... done in 0:00:01.001239
+
+  [Example 2] timer with description
+  ...Waiting
+  ...Waiting done in 0:00:01.001262
+
+  [Example 3] timer without starting message
+  ...Waiting done in 0:00:01.001249
+
+  [Example 4] timer without ending message
+  ...Waiting
+
+  [Example 5] rewrite the same line after ending
+  ...Waiting done in 0:00:01.001291
+
+  [Example 6] change the prefix
+  [bistimer] Waiting
+  [bistimer] Waiting done in 0:00:01.001201
+
+  [Example 7] use python logging module
+  [2016-10-17 22:49:47,121] INFO: __main__: ...Waiting
+  [2016-10-17 22:49:48,123] INFO: __main__: ...Waiting done in 0:00:01.001263
+
+  [Example 8] use python logging module with different log level
+  [2016-10-17 22:49:48,123] DEBUG: __main__: ...Waiting
+  [2016-10-17 22:49:49,125] DEBUG: __main__: ...Waiting done in 0:00:01.001245
+  ```
--- examples/simple_timing_example.py
+++ examples/simple_timing_example.py
@@ -1,19 +1,46 @@
 from time import sleep
+import logging
 
 from bistiming import SimpleTimer
 
-print("[Example 1]")
-with SimpleTimer():
-    sleep(1)
 
-print("\n[Example 2] timer with description")
-with SimpleTimer("Waiting"):
-    sleep(1)
+def main():
+    print("[Example 1]")
+    with SimpleTimer():
+        sleep(1)
 
-print("\n[Example 3] timer without starting message")
-with SimpleTimer("Waiting", verbose_start=False):
-    sleep(1)
+    print("\n[Example 2] timer with description")
+    with SimpleTimer("Waiting"):
+        sleep(1)
 
-print("\n[Example 4] timer without ending message")
-with SimpleTimer("Waiting", verbose_end=False):
-    sleep(1)
+    print("\n[Example 3] timer without starting message")
+    with SimpleTimer("Waiting", verbose_start=False):
+        sleep(1)
+
+    print("\n[Example 4] timer without ending message")
+    with SimpleTimer("Waiting", verbose_end=False):
+        sleep(1)
+
+    print("\n[Example 5] rewrite the same line after ending")
+    with SimpleTimer("Waiting", end_in_new_line=False):
+        sleep(1)
+
+    print("\n[Example 6] change the prefix")
+    with SimpleTimer("Waiting", prefix="[bistimer] "):
+        sleep(1)
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="[%(asctime)s] %(levelname)s: %(name)s: %(message)s")
+    logger = logging.getLogger(__name__)
+
+    print("\n[Example 7] use python logging module")
+    with SimpleTimer("Waiting", logger=logger):
+        sleep(1)
+
+    print("\n[Example 8] use python logging module with different log level")
+    with SimpleTimer("Waiting", logger=logger, logging_level=logging.DEBUG):
+        sleep(1)
+
+if __name__ == '__main__':
+    main()
